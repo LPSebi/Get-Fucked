ec65365f1e35bf721e4f590fd90a8a2aac670010
LPSebi <52174534+LPSebi@users.noreply.github.com>
2021-09-12 15:09:32 +0200

    Flake8 updates

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ from time import sleep
 # <---------->
 
 LOGIN_MSG = r"""
- ______     ______     ______      ______   __  __     ______     __  __     ______     _____    
-/\  ___\   /\  ___\   /\__  _\    /\  ___\ /\ \/\ \   /\  ___\   /\ \/ /    /\  ___\   /\  __-.  
-\ \ \__ \  \ \  __\   \/_/\ \/    \ \  __\ \ \ \_\ \  \ \ \____  \ \  _"-.  \ \  __\   \ \ \/\ \ 
- \ \_____\  \ \_____\    \ \_\     \ \_\    \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\  \ \____- 
-  \/_____/   \/_____/     \/_/      \/_/     \/_____/   \/_____/   \/_/\/_/   \/_____/   \/____/ 
+ ______     ______     ______      ______   __  __     ______     __  __     ______     _____
+/\  ___\   /\  ___\   /\__  _\    /\  ___\ /\ \/\ \   /\  ___\   /\ \/ /    /\  ___\   /\  __-.
+\ \ \__ \  \ \  __\   \/_/\ \/    \ \  __\ \ \ \_\ \  \ \ \____  \ \  _"-.  \ \  __\   \ \ \/\ \
+ \ \_____\  \ \_____\    \ \_\     \ \_\    \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\  \ \____-
+  \/_____/   \/_____/     \/_/      \/_/     \/_____/   \/_____/   \/_/\/_/   \/_____/   \/____/
 
 """  # http://patorjk.com/software/taag/#p=display&f=Sub-Zero&t=GET%20FUCKED
 
@@ -28,15 +28,15 @@ def login():
     """Print login msg"""
     print(LOGIN_MSG)
 
-#<---------->
+# <---------->
 
 
-def hide(): # USE PYINSTALLER "--window" OPTION (https://stackoverflow.com/questions/49648642/convert-pyw-to-exe-and-have-the-command-line-not-open)
+def hide():  # USE PYINSTALLER "--window" OPTION (https://bit.ly/3C6myWa)
     """Hide cmd window"""
     thisprogramm = win32gui.GetForegroundWindow()
     win32gui.ShowWindow(thisprogramm, win32con.SW_HIDE)
 
-#<---------->
+# <---------->
 
 
 def spam_msgbox():
